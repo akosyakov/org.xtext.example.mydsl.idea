a8cfe2cb5721ba3cc56d78dcc9fbc20a04d376cc
akosyakov <anton.kosyakov@itemis.de>
2014-08-18 16:15:47 +0200

    removed synchronization for files to refresh

--- org.xtext.idea.builder/src/org/xtext/idea/build/XtextIdeaRefreshComponent.java
+++ org.xtext.idea.builder/src/org/xtext/idea/build/XtextIdeaRefreshComponent.java
@@ -2,28 +2,19 @@ package org.xtext.idea.build;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
 
 public class XtextIdeaRefreshComponent {
-	private final Object lock;
     private final Collection<String> filesToReferesh;
     
     public XtextIdeaRefreshComponent() {
-    	lock = new Object();
 		filesToReferesh = new HashSet<String>();
 	}
 
     public void refresh(String path) {
-        synchronized (lock) {
-        	filesToReferesh.add(path);
-        }
+        filesToReferesh.add(path);
     }
 
     public Collection<String> getFilesToRefresh() {
-        Set<String> result = new HashSet<String>();
-        synchronized (lock) {
-            result.addAll(filesToReferesh);
-        }
-        return result;
+        return filesToReferesh;
     }
 }
